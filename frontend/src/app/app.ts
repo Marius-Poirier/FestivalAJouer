bff38ac1db315f21f25c50e5372284b20e5aeb42
Fatima Abair <fatima-azahara.abair-naimi@etu.umontpellier.fr>
2025-11-25 12:28:53 +0100

    feat(header): implementation du composant header

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '@auth/auth-services';
+import { Header } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
