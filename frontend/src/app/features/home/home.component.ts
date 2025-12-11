26840ff7a3fd71a67dd9527f7ac626af5a2fa6f1
Fatima Abair <fatima-azahara.abair-naimi@etu.umontpellier.fr>
2025-12-11 20:21:55 +0100

    feat(home): création de la page home

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,20 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from 'src/app/core/services/auth-services';
+import { FestivalService } from '@festivals/services/festival-service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatIconModule, DatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   protected readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  protected readonly festivalssvc = inject(FestivalService)
+  protected readonly festival = this.festivalssvc.lastFestival
 
   ngOnInit(): void {
-    // Verify session on component init
     if (!this.authService.currentUser()) {
       this.authService.whoami();
     }
@@ -22,11 +25,12 @@ export class HomeComponent implements OnInit {
 
   protected onLogout(): void {
     this.authService.logout();
-    // Navigate to login after logout
     setTimeout(() => {
       if (!this.authService.isLoggedIn()) {
         this.router.navigate(['/login']);
       }
     }, 500);
   }
+
+
 }
