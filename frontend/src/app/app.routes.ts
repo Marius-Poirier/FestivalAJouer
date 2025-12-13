5bd839be9d83566466c4bb2d02dc9d564421e8e9
Fatima Abair <fatima-azahara.abair-naimi@etu.umontpellier.fr>
2025-12-13 19:24:12 +0100

    feat(home): amélioration de la page home et selector

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,49 +1,52 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '@core/guards/auth-guard';
 import { adminGuard } from '@core/guards/admin-guard';
-import { FestivalList } from '@festivals/components/festival-list/festival-list';
-import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
     {
-    path: 'login',
-    loadComponent: () => import('@auth/login/login').then(m => m.Login)
+        path: 'login',
+        loadComponent: () => import('@auth/login/login').then(m => m.Login)
     },
     {
-    path: 'home',
-    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
     },
     {
-    path: 'admin',
-    loadComponent: () => import('@admin/admin').then(m => m.AdminComponent),
-    canActivate: [adminGuard]
+        path: 'admin',
+        loadComponent: () => import('@admin/admin').then(m => m.AdminComponent),
+        canActivate: [adminGuard]
     },
     {
-    path: 'register',
-    loadComponent: () => import('@auth/register/register').then(m => m.Register),
-    },
-    // {
-    // path: 'festivals',
-    // loadComponent: () => import('@festivals/components/festival-list/festival-list').then(m => m.FestivalList),
-    // canActivate: [authGuard]
-    // },
-    {
-    path: 'festivals',
-    component: HomeComponent,
+        path: 'register',
+        loadComponent: () => import('@auth/register/register').then(m => m.Register),
     },
     {
-    path: 'editeurs',
-    loadComponent: () => import('@editeurs/components/editeur-list/editeur-list').then(m => m.EditeurList),
-    canActivate: [authGuard]
+        path: 'festivals',
+        loadComponent: () => import('@festivals/components/festival-list/festival-list').then(m => m.FestivalList),
+        canActivate: [authGuard]
     },
     {
-    path: 'editeurs/:id',
-    loadComponent: () => import('@editeurs/components/editeur-detail/editeur-detail').then(m => m.EditeurDetail),
-    canActivate: [authGuard]
+        path: 'editeurs',
+        loadComponent: () => import('@editeurs/components/editeur-list/editeur-list').then(m => m.EditeurList),
+        canActivate: [authGuard]
     },
     {
-    path: 'workflow',
-    loadComponent: ()=> import('@festivals/components/workflow-festival/workflow-festival').then(m=>m.WorkflowFestival)
+        path: 'editeurs/:id',
+        loadComponent: () => import('@editeurs/components/editeur-detail/editeur-detail').then(m => m.EditeurDetail),
+        canActivate: [authGuard]
+    },
+    {
+        path: 'workflow',
+        loadComponent: () => import('@festivals/components/workflow-festival/workflow-festival').then(m => m.WorkflowFestival),
+        canActivate: [authGuard]
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
     }
-
 ];
