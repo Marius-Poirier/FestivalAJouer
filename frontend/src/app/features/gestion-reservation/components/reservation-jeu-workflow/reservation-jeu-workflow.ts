a5147a41104bfb9b6c96be7f26c1c0302a08c280
ismael-bny <ismael.bny@gmail.com>
2026-01-19 17:40:58 +0100

    feat: filtres workflow jeux

--- frontend/src/app/features/gestion-reservation/components/reservation-jeu-workflow/reservation-jeu-workflow.ts
+++ frontend/src/app/features/gestion-reservation/components/reservation-jeu-workflow/reservation-jeu-workflow.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, computed } from '@angular/core';
 import { Router } from '@angular/router';
 import { JeuCard } from '@jeux/components/jeu-card/jeu-card';
 import { GestionReservationService } from '@gestion-reservation/services/gestion-reservation-service';
 import { CurrentFestival } from '@core/services/current-festival';
 import { JeuDto } from '@interfaces/entites/jeu-dto';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-workflow-jeux',
   standalone: true,
-  imports: [CommonModule, JeuCard],
+  imports: [CommonModule, JeuCard, MatIconModule],
   templateUrl: './reservation-jeu-workflow.html',
   styleUrl: './reservation-jeu-workflow.css'
 })
@@ -19,12 +20,30 @@ export class ReservationJeuWorkFlow {
   private readonly router = inject(Router);
 
   protected readonly currentFestival = this.currentFestivalSvc.currentFestival;
-
   protected readonly isLoading = this.gestionSvc.isLoading;
   protected readonly error = this.gestionSvc.error;
 
-  // on stocke les jeux complets
+  // jeux complets
   protected readonly jeux = signal<JeuDto[]>([]);
+
+  // ===== barre de recherche =====
+  protected readonly searchTerm = signal<string>('');
+
+  protected onSearch(event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    this.searchTerm.set(target?.value ?? '');
+  }
+
+  protected readonly filteredJeux = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+    const list = this.jeux();
+
+    if (!term) return list;
+
+    return list.filter(j =>
+      (j.nom ?? '').toLowerCase().includes(term)
+    );
+  });
 
   constructor() {
     effect(() => {
@@ -34,7 +53,6 @@ export class ReservationJeuWorkFlow {
         return;
       }
 
-      // charge les jeux complets (mêmes infos que la page /jeux)
       this.gestionSvc.loadJeuxCompletsPourFestival(festId).subscribe((list) => {
         this.jeux.set(list ?? []);
       });
